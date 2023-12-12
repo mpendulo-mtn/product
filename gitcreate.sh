183cb480905a5c6f1aa1e4aa1c9c48916edd2824
mpendulo-mtn <151135548+mpendulo-mtn@users.noreply.github.com>
2023-12-12 12:19:31 +0200

    Generating new folder structure for Microservices

--- gitcreate.sh
+++ gitcreate.sh
@@ -15,7 +15,7 @@ git commit -m "Generating new folder structure for Microservices"
 
 git remote add origin https://github.com/mpendulo-mtn/$name.git
 
-git push -f origin master
+git push -f origin main
 
 git checkout -b dev
 
@@ -25,6 +25,6 @@ git checkout -b stage
 
 git push -u origin stage
 
-git checkout master
+git checkout main
 
 bash
